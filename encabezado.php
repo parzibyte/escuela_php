fc595f72ba5fa7ffe9a406588509ad6ff56333d1
Luis Cabrera Benito <parzibyte@gmail.com>
2020-09-28 00:35:31 -0500

    Menú

--- encabezado.php
+++ encabezado.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap.min.css">
+
+    <style>
+        body {
+            padding-top: 70px;
+        }
+    </style>
     <title>Control de notas</title>
 </head>
-<body>
-<main class="container-fluid">
 
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-success fixed-top">
+        <a class="navbar-brand" href="#">Control de notas</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrar_estudiantes.php">Estudiantes</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="mostrar_materias.php">Materias</a>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+    <main class="container-fluid">
